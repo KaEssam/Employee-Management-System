a4c5564b397662ce112b3cdfe3baa430461fedfe
KaEssam <Kaessam17@gmail.com>
2024-04-21 13:51:34 +0200

    Add project files.

---
+++ ServerLibrary/DataBase/AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using BaseLibrary.Entites;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerLibrary.DataBase
+{
+    public class AppDbContext(DbContextOptions options) : DbContext(options)
+    {
+        public DbSet<Employee> Employees { get; set; }
+
+        public DbSet<GeneralDepartment> GeneralDepartments { get; set; }
+
+        public DbSet<Department> Departments { get; set; }
+
+        public DbSet<Branch> Branches { get; set; }
+
+        public DbSet<Town> Towns { get; set; }
+
+        public DbSet<AppUser> AppUsers { get; set; }
+    }
+}
